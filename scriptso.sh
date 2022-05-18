3dfd537e7e2b7125f3c35825b398b1c61c5d1be6
Viorel Tudor <viorel.tudor@webtailors.ro>
2022-05-18 20:42:21 +0300

    add option docker version

--- scriptso.sh
+++ scriptso.sh
@@ -3,13 +3,12 @@
 # Main menu of the project codename RaisingPrinces
 # This project is not ment to be used in real life.
 # Authors legacy for Operating Systems course at Bucharest University of Economic Studies 
-# Supervisor: Antonio Clim
 # For details about this project read "readme.txt"
 
 title="Welcome to RaisingPrinces app"
 isFirstSelection=0
 prompt="Please, pick option from below:"
-options=("User guide" "Secure password generator" "Show all docker containers" "Check docker version" "Restart docker instances" "Quit")
+options=("User guide" "Secure password generator" "Check docker version" "Show all docker containers" "Restart docker instances" "Quit")
 
 GetDockerContainersList() {
     output=$(eval "docker ps -a --format '{{.ID}} {{.Names}}'")
@@ -18,6 +17,7 @@ GetDockerContainersList() {
     read -rd '' -a CONTAINERS <<<"$output"
 }
 
+#### BEGIN INTERACTION #####
 echo "$title"
 # use promt shell 3 to capture user selected option
 PS3="$prompt "
@@ -48,21 +48,57 @@ do
             echo "$(( $RANDOM % ${#alphabet[@]} + 1 ))"
         done 
         ;;
+        ### NEXT OPTION ###
         "Check docker version")
         # check if docker is installed
         # if YES -> then show version
         # if NO -> promt user to install
-        echo "Checking docker version"
+        echo 'Checking docker version'
+            dockerPath=$(which docker)
+            dockerVersion=$(docker -v)
+            if [[ $dockerPath && $dockerVersion ]]
+            then
+                echo "### Docker version: $dockerVersion installed on $dockerPath. All good to go!"
+            else
+                echo "### Docker not found on this system."
+                # we put -n param because want user coursor to stay inline with text
+                # displayed on stdout
+                echo -n "### Shall we install it[Y(es)/N(o)]? Pick Yes only if are using Ubuntu: ";
+                read;
+                userSelectedValue=${REPLY}
+                # user typed character is then conducted through pipe operator 
+                # and gets applied a REGEX expression that match any capital letter 
+                # to the lower case variant 
+                userSelectedValue=$userSelectedValue | sed -e 's/\(.*\)/\L\1/'
+                echo $userSelectedValue
+                
+                if [[ $userSelectedValue == "yes" || $userSelectedValue == 'y' ]]; then
+                # we can put this kind of instructions in a variable 
+                # because why may want to detect user system and to apply specific instructions
+                # NOTICE 1) sudo -s argument = run the shell specified by the SHELL env
+                #          -- sudo should stop processing command line arguments
+                # NOTICE 2) the -y argument = answer with yes automatic
+                sudo -s -- <<EOF
+                apt-get update 
+                apt-get install -y linux-image-extra-$(uname -r)
+                apt-get install docker-engine -y
+                service docker start
+                docker run hello-world
+EOF
+                else
+                    echo "No option"
+                fi
+            fi
         ;;
-
+        ### NEXT OPTION ###
         "Show all docker containers")
-        echo "Docker containers"
-        GetDockerContainersList
-        printf "Number of containers: %d\n" "${#CONTAINERS[@]}"
-        for container in "${CONTAINERS[@]}"
-        do
-            : 
-            echo $container
+            echo "Docker containers"
+            GetDockerContainersList
+            printf "Number of containers: %d\n" "${#CONTAINERS[@]}"
+            for container in "${CONTAINERS[@]}"
+            do
+                : 
+                echo $container
         done
         ;;
         
